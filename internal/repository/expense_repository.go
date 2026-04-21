9435ffd5bf5700619c6f6d8e82ca5b9416b4f2f5
Dhinesh K <102824222+dhineshkrishna@users.noreply.github.com>
2026-04-22 00:04:02 +0530

    Update expense_repository.go

--- internal/repository/expense_repository.go
+++ internal/repository/expense_repository.go
@@ -5,5 +5,5 @@ import "expense-tracker/internal/domain"
 type ExpenseRepository interface {
 	Create(exp domain.Expense) error
 	Get(category, from, to, sort string) ([]domain.Expense, float64, error)
-	GetSummary(userID string) (map[string]float64, error)
+	GetSummary() (map[string]float64, error)
 }
